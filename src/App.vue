6053b014af7d85f1fc818e6cee281d5792ee2d1e
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2022-12-02 17:27:40 +0100

    creazione nav

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import theHeader from './components/theHeader.vue';
+import theFooter from "./components/theFooter.vue";
+export default {
+  components: { theHeader, theFooter }
+};
+</script>
+<template>
+  <theHeader></theHeader>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/theHeader.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+};
+</script>
+<template>
+    <header>
+        <div class="container">
+            <nav class="mt-3">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <img src="/dc-logo.png" alt="">
+                    </div>
+                    <div>
+                        <ul class="d-flex">
+                            <li>CHARACTERS</li>
+                            <li class="active">COMICS</li>
+                            <li>MOVIES</li>
+                            <li>TV</li>
+                            <li>GAMES</li>
+                            <li>COLLECTIBLES</li>
+                            <li>VIDEOS</li>
+                            <li>FANS</li>
+                            <li>NEWS</li>
+                            <li>SHOP</li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+
+
+<style scoped>
+li {
+    padding: 0 .35rem;
+    list-style: none;
+}
+.active{
+    color: blue;
+    border-bottom: 2px solid blue;
+}
+</style>
